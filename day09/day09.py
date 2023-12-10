cd0a1c94cb380e6083516ad819e2a033fa965b11
Martin HA <martin.herrerias@gmail.com>
2023-12-10 01:32:36 +0000

    Day 9 part 1

---
+++ day09/day09.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+'''
+Advent of Code 2023 - Day09
+
+'''
+
+import sys
+import argparse
+import inspect
+import re
+import numpy as np
+
+def parse_line(line, part = 2, verbose = False):
+    numbers = np.array(list(map(int, re.findall(r'([-\d]+)', line))))
+    next = predict(numbers, verbose)
+    return next
+        
+def predict(numbers, verbose):
+    d = np.diff(numbers)
+    if all(d == d[0]):
+        next = numbers[-1] + d[0]
+    else:
+        next = numbers[-1] + predict(d, verbose)
+    if verbose:
+        print(f'{numbers} >> {next}')
+    return next
+
+def parse_args():
+    parser = argparse.ArgumentParser(usage= '''Usage:
+        cat data.txt | day09.py [-v] [--part 1]
+        python3 -m day09.py --file data.txt [-v] [--part 1]''')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+    parser.add_argument('--part', type=int, default=2, help='Specify the part number')
+    parser.add_argument('--file', type=str, help='Specify input file')
+    return parser.parse_args()
+
+def main(args):
+
+    if args.file:
+        with open(args.file) as f:
+            lines = f.readlines()
+    else:
+        lines = sys.stdin.readlines()
+
+    values = [parse_line(line, args.part, args.verbose) for line in lines]
+    total = sum(values)
+
+    frame = inspect.currentframe().f_back
+    if frame.f_code.co_nlocals > 0:
+        return total
+    else:
+        print(f'Score: {total}')
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
+
---
+++ day09/day09_test.py
@@ -0,0 +1,40 @@
+# Unit tests for src/day09.py
+
+import unittest
+from argparse import Namespace
+
+from day09 import *
+
+import os
+here = os.path.dirname(os.path.realpath(__file__))
+test_data = os.path.join(here, 'day09.test.dat')
+full_data = os.path.join(here, 'day09.dat')
+
+class TestDay09(unittest.TestCase):
+
+    def test_parse_line(self):
+        self.assertEqual(parse_line('0 3 6 9 12 15', 1, False), 18)
+        self.assertEqual(parse_line('1 3 6 10 15 21', 1, False), 28)
+        self.assertEqual(parse_line('10 13 16 21 30 45', 1, False), 68)
+        self.assertEqual(parse_line('-3 -1 1 3', 1,False),5)
+
+    def test_part_1(self):
+        args = Namespace(file = test_data, part = 1, verbose = False)
+        self.assertEqual(main(args), 114)
+
+    def test_part_1_full(self):
+        args = Namespace(file = full_data, part = 1, verbose = False)
+        self.assertEqual(main(args), 1757008019)
+
+    @unittest.skip("Skipping part 2")
+    def test_part_2(self):
+        args = Namespace(file = test_data, part = 2, verbose = False)
+        self.assertEqual(main(args), 360)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
