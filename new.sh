a6cd439567b8fb53b40f8f0cdaa7e7f586e2ce64
Martin HA <martin.herrerias@gmail.com>
2023-12-10 01:17:41 +0000

    Fix new.sh for the new tree structure

--- new.sh
+++ new.sh
@@ -1,21 +1,20 @@
 #! /usr/bin/env bash
 
 day=$1
-if [ $day -lt 10 ]
-then
-    day=0${day}
-fi
+if [ $day -lt 10 ]; then day=0${day##*0}; fi
 day=day${day}
 
-touch data/${day}.txt
-echo foo > test_data/${day}.txt
-cp src/template.py src/${day}.py
-cp src/template_test.py src/${day}_test.py
+mkdir ${day}
+for f in template/*; do
+    f=`basename ${f}`
+    echo "cp template/$f ${day}/${day}${f##template}"
+    cp template/$f ${day}/${day}${f##template}
+done
 
 # Replace 'template' with $day in src/${day}_*.py
-sed -i "s/template/${day}/g" src/${day}.py
-sed -i "s/template/${day}/g" src/${day}_test.py
+sed -i "s/template/${day}/g" ${day}/${day}.py
+sed -i "s/template/${day}/g" ${day}/${day}_test.py
 
 # Replace 'Template' with capitalized $day in src/${day}_*.py
-sed -i "s/Template/${day^}/g" src/${day}.py
-sed -i "s/Template/${day^}/g" src/${day}_test.py
+sed -i "s/Template/${day^}/g" ${day}/${day}.py
+sed -i "s/Template/${day^}/g" ${day}/${day}_test.py
