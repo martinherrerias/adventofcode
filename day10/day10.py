7ac9daa0403719a6bd5b2e3098e16f7fbc4529a9
Martin HA <martin.herrerias@gmail.com>
2023-12-10 16:26:34 +0000

    Day 10, part 1, recursive

---
+++ day10/day10.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python3
+
+'''
+Advent of Code 2023 - Day10
+
+'''
+
+import sys
+import argparse
+import inspect
+import re
+import numpy as np
+
+from collections import namedtuple
+
+Tile = namedtuple('TileType', ['key', 'N', 'S','E','W'])
+def get_tile(key):
+    return {
+        '|': Tile('|', True, True, False, False),  # vertical pipe
+        '-': Tile('-', False, False, True, True),  # horizontal pipe
+        'L': Tile('L', True, False, True, False),  # 90-degree bend (N, E)
+        'J': Tile('J', True, False, False, True),  # 90-degree bend (N, W)
+        '7': Tile('7', False, True, False, True),  # 90-degree bend (S, W)
+        'F': Tile('F', False, True, True, False),  # 90-degree bend (S, E)
+        '.': Tile('.', False, False, False, False),  # ground
+        'S': Tile('S', True, True, True, True),  # starting position
+    }[key]
+
+def connections(tiles, i, j):
+
+    assert isinstance(tiles, list)
+    assert isinstance(tiles[i], list)
+    assert isinstance(tiles[i][j], Tile)
+
+    t = tiles[i][j]
+    if i > 0 and t.N and tiles[i-1][j].S:
+        yield (i-1, j)
+    if i < len(tiles)-1 and t.S and tiles[i+1][j].N:
+        yield (i+1, j)
+    if j > 0 and t.W and tiles[i][j-1].E:
+        yield (i, j-1)
+    if j < len(tiles[0])-1 and t.E and tiles[i][j+1].W:
+        yield (i, j+1)
+
+def get_distances(tiles, verbose=False):
+
+    assert isinstance(tiles, list)
+    assert isinstance(tiles[0], list)
+    assert isinstance(tiles[0][0], Tile)
+
+    D = np.zeros((len(tiles), len(tiles[0])), dtype=int)
+    D.fill(-1)
+
+    def propagate(i, j, dist = 0):
+
+        assert isinstance(i, int)
+        assert isinstance(j, int)
+
+        nonlocal D
+        nonlocal tiles
+
+        c = list(connections(tiles, i, j))
+        if verbose:
+            print(f'Found {len(c)} connections for {tiles[i][j].key}@({i},{j}): {c}')
+
+        for ci, cj in c:
+            if D[ci,cj] == -1 or D[ci,cj] > dist + 1:
+
+                D[ci,cj] = dist + 1
+                propagate(ci, cj, dist + 1)
+
+                if verbose:
+                    print(f'Going into {tiles[ci][cj].key}@({ci},{cj}) with dist {dist+1}')
+        
+    start = next((i, j) for i, row in enumerate(tiles) for j, tile in enumerate(row) if tile.key == 'S')
+    D[start] = 0
+
+    if verbose:
+        print(f'Starting at {start}')
+
+    propagate(*start)
+
+    return D
+
+def parse_args():
+    parser = argparse.ArgumentParser(usage= '''Usage:
+        cat data.txt | day10.py [-v] [--part 1]
+        python3 -m day10.py --file data.txt [-v] [--part 1]''')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+    parser.add_argument('--part', type=int, default=2, help='Specify the part number')
+    parser.add_argument('--file', type=str, help='Specify input file')
+    return parser.parse_args()
+
+def main(args):
+
+    if args.file:
+        with open(args.file) as f:
+            lines = f.readlines()
+    else:
+        lines = sys.stdin.readlines()
+
+    tiles = [[get_tile(c) for c in line.strip()] for line in lines]
+
+    D = get_distances(tiles, args.verbose)
+
+    total = np.max(D)
+
+    frame = inspect.currentframe().f_back
+    if frame.f_code.co_nlocals > 0:
+        return total
+    else:
+        print(f'Score: {total}')
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
+
---
+++ day10/day10_test.py
@@ -0,0 +1,82 @@
+# Unit tests for src/day10.py
+
+import unittest
+from argparse import Namespace
+import numpy as np
+
+from day10 import *
+
+import os
+import glob
+import re
+
+here = os.path.dirname(os.path.realpath(__file__))
+test_data = glob.glob(os.path.join(here, '*.test.dat'))
+keys = [re.match(r'.*(\w).test.dat', f).group(1) for f in test_data]
+test_data = dict(zip(keys, test_data))
+
+class TestDay10(unittest.TestCase):
+
+    def test_connections(self):
+        input = np.array([
+            ['S', '-', '7'],
+            ['|', '.', '|'],
+            ['L', '-', 'J']
+        ])
+        tiles = [[get_tile(c) for c in line] for line in input]
+        self.assertEqual(list(connections(tiles, 0, 0)), [(1,0), (0,1)])
+        self.assertEqual(list(connections(tiles, 0, 1)), [(0,0), (0,2)])
+        self.assertEqual(list(connections(tiles, 0, 2)), [(1,2), (0,1)])
+        self.assertEqual(list(connections(tiles, 1, 0)), [(0,0), (2,0)])
+        self.assertEqual(list(connections(tiles, 1, 1)), [])
+        self.assertEqual(list(connections(tiles, 1, 2)), [(0,2), (2,2)])
+        self.assertEqual(list(connections(tiles, 2, 0)), [(1,0), (2,1)])
+        self.assertEqual(list(connections(tiles, 2, 1)), [(2,0), (2,2)])
+        self.assertEqual(list(connections(tiles, 2, 2)), [(1,2), (2,1)])
+
+    def test_get_distances(self):
+        with open(test_data['a']) as f:
+            tiles = [[get_tile(c) for c in line.strip()] for line in f.readlines()]
+        D = get_distances(tiles)
+        self.assertEqual(D.tolist(), [[0, 1, 2],[1, -1, 3],[2, 3, 4]])
+
+    def test_get_distances(self):
+        with open(test_data['b']) as f:
+            tiles = [[get_tile(c) for c in line.strip()] for line in f.readlines()]
+        D = get_distances(tiles)
+        self.assertEqual(D.tolist(), [
+            [-1,-1,-1,-1,-1],
+            [-1, 0, 1, 2,-1],
+            [-1, 1,-1, 3,-1],
+            [-1, 2, 3, 4,-1],
+            [-1,-1,-1,-1,-1]])
+        
+    def test_get_distances(self):
+        with open(test_data['d']) as f:
+            tiles = [[get_tile(c) for c in line.strip()] for line in f.readlines()]
+        D = get_distances(tiles)
+        self.assertEqual(D.tolist(), [
+            [-1,-1, 4, 5,-1],
+            [-1, 2, 3, 6,-1],
+            [ 0, 1,-1, 7, 8],
+            [ 1, 4, 5, 6, 7],
+            [ 2, 3,-1,-1,-1]])
+
+    def test_part_1(self):
+        answ = {'a': 4, 'b': 4, 'c': 8, 'd': 8}
+        for k, ans in answ.items():
+            args = Namespace(file = test_data[k], part = 1, verbose = False)
+            self.assertEqual(main(args), ans)
+
+    @unittest.skip('not implemented')
+    def test_part_2(self):
+        args = Namespace(file = test_data['a'], part = 2, verbose = False)
+        self.assertEqual(main(args), 360)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
