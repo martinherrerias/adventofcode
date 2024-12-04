20409c28d35ee7c90023e7ebbf5d4033b23b0599
Martin HA <martin.herrerias@gmail.com>
2024-12-04 18:29:42 +0000

    simplify template

--- template/template.py
+++ template/template.py
@@ -1,26 +1,22 @@
 #! /usr/bin/env python3
-
 '''
 Advent of Code 2024 - https://adventofcode.com/2024/
 '''
 
-import sys
 import argparse
-import inspect
 from pathlib import Path
 
 import numpy as np
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        usage=f'python3 {Path(__file__).name} [--file DATA] [--part N] [-v]'
-              f'cat DATA | {Path(__file__).name} [--part 1] [-v]'
-    )
-    parser.add_argument('--file', type=str, default=Path(__file__).with_suffix('.dat').name, 
-        help='Specify input file, default: %(default)s')
-    parser.add_argument('--part', type=int, default=1, help='Part number, default: %(default)s')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, default=Path(__file__).with_suffix('.dat'), 
+        help='Input file, default: %(default)s')
+    parser.add_argument('--part', type=int, nargs='+', default=[1, 2],
+        help='Part number(s), default: %(default)s')
+    parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
+
 
 def part_1(row):
     return sum(row)
@@ -28,14 +24,13 @@ def part_1(row):
 def part_2(row):
     return sum(row) * 2
 
+
 def main(file=None, part=None, verbose=False):
 
-    if file:
-        with open(file, encoding='utf-8') as f:
-            lines = f.readlines()
-    else:
-        lines = sys.stdin.readlines()
+    with open(file, encoding='utf-8') as f:
+        lines = f.readlines()
 
+    # Numeric row-wise
     rows = [np.fromstring(line, dtype=int, sep=' ') for line in lines]
 
     if part == 1:
@@ -47,16 +42,26 @@ def main(file=None, part=None, verbose=False):
 
     total = sum(values)
 
+    # # Generic block-wise
+    # data = np.array([list(line.strip()) for line in lines])
+
+    # if part == 1:
+    #     total = part_1(data)
+    # elif part == 2:
+    #     total = part_2(data)
+    # else:
+    #     raise ValueError(f'Invalid part number: {part}')
+    
     if verbose:
         for i, value in enumerate(values):
             print(f'{rows[i]}: {value}')
 
-    frame = inspect.currentframe().f_back
-    if frame.f_code.co_nlocals > 0:
-        return total
-    else:
-        print(f'Score: {total}')
+    return total
+
 
 if __name__ == '__main__':
+
     args = parse_args()
-    main(**vars(args))
+    for part in args.part:
+        total = main(args.file, part, args.verbose)
+        print(f'Part {part} total: {total}')
