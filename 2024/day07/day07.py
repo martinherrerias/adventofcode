9f19725ebde192747b2d8f5c18959df8577c4e88
Martin HA <martin.herrerias@gmail.com>
2024-12-07 13:23:35 +0000

    Day 7: Small performance improvement

--- 2024/day07/day07.py
+++ 2024/day07/day07.py
@@ -6,10 +6,16 @@ Advent of Code 2024 - https://adventofcode.com/2024/
 import argparse
 from pathlib import Path
 import operator
+import time
 
 OPERATORS = {'+': operator.add,
              '*': operator.mul,
              '||': lambda a, b: int(str(a) + str(b))}
+
+# For sequences of a certain size, it should be faster too calculate upper
+# and lower bounds, and check whether it's worth continuing.
+# Using 2 gives the best peformance improvement for 3 operations (20-25%) 
+TEST_BOUNDS_IF_LEN = 2
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,9 +23,21 @@ def parse_args():
         help='Input file, default: %(default)s')
     parser.add_argument('--part', type=int, nargs='+', default=[1, 2],
         help='Part number(s), default: %(default)s')
+    parser.add_argument('--K', type=int, default = 2,
+        help='Test upper/lower bounds instead of exhaustive search for N > K')
     parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
+def bounds(numbers, ops = ['+','*']):
+
+    lower = numbers[0]
+    upper = numbers[0]
+
+    for j in range(1, len(numbers)):
+        lower = min([OPERATORS[op](lower, numbers[j]) for op in ops])
+        upper = max([OPERATORS[op](upper, numbers[j]) for op in ops])
+
+    return lower, upper
 
 def test(target, numbers, ops = ['+','*']):
 
@@ -28,6 +46,13 @@ def test(target, numbers, ops = ['+','*']):
             return prev == target
         
         prev = op(prev, numbers[next])
+
+        if len(numbers) - next - 2 > TEST_BOUNDS_IF_LEN:
+            lower, upper = bounds([prev] + numbers[next+1:], ops)
+            if lower == target or upper == target:
+                return True
+            if not (lower < target < upper):
+                return False
 
         for op in ops:
             if _test(prev, OPERATORS[op], next + 1):
@@ -78,6 +103,11 @@ def main(file=None, part=None, verbose=False):
 if __name__ == '__main__':
 
     args = parse_args()
+    TEST_BOUNDS_IF_LEN = args.K
+
+    t0 = time.time()
     for part in args.part:
         total = main(args.file, part, args.verbose)
         print(f'Part {part} total: {total}')
+
+    print(f'Testing bounds for N > {TEST_BOUNDS_IF_LEN}, elapsed: {time.time() - t0}')
