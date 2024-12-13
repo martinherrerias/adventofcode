9d922926b5098929c16446d4cee7a265af9e9fa0
Martin HA <martin.herrerias@gmail.com>
2024-12-13 08:45:12 +0000

    Day 12, part 2

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -11,7 +11,7 @@ from collections import namedtuple
 
 PADDING = '.'
 
-Region = namedtuple('Region', ['tag', 'area', 'perimeter'])
+Region = namedtuple('Region', ['tag', 'area', 'perimeter', 'sides'])
 
 def next_seed(used):
     for index, isused in np.ndenumerate(used):
@@ -21,16 +21,24 @@ def next_seed(used):
 
 def find_regions(data):
 
+    r_offset = np.array([1, -1, 0, 0])
+    c_offset = np.array([0, 0, -1, 1])
+    DOWN, UP, LEFT, RIGHT = 0, 1, 2, 3
+
     used = np.zeros(data.shape, bool)
+
     data = np.pad(data, 1, constant_values=PADDING)
     used = np.pad(used, 1, constant_values=True)
 
-    r_offset = np.array([1, -1, 0, 0])
-    c_offset = np.array([0, 0, -1, 1])
+    edges = np.zeros(data.shape + (4,), bool) # one for each direction
 
-    def grow(r0, c0, a = 0, p = 0):
+    def grow(r0, c0, a = 0, p = 0, s = 0):
 
         nonlocal used
+        nonlocal edges
+
+        if a == 0:
+            edges[:] = False  # reset edges for each new patch
 
         used[r0, c0] = True
         a += 1
@@ -41,52 +49,76 @@ def find_regions(data):
         foreign = data[next_r, next_c] != data[r0, c0]
         p += sum(foreign)
 
+        # add edges & counter-edges
+        for d in np.nonzero(foreign)[0]:
+            edges[r0, c0, d] = True
+            edges[next_r[d], next_c[d], d ^ 1] = True
+
+        # figure out if new edge creates/continues/deletes sides
+        def side_count(direction, neighbors):
+
+            nonlocal s
+            nonlocal edges
+
+            if not foreign[direction]:
+                return
+
+            # check if neighbors share edge
+            extensions = sum(edges[next_r[neighbors], next_c[neighbors], direction])
+
+            if extensions == 0:    # edge creates new side
+                    s += 1
+            elif extensions == 2:  # edge merges two existing sides
+                s -= 1  
+
+            # check if next cell shares perpendicular edges
+            crossings = sum(
+                edges[next_r[direction], next_c[direction], neighbors] & \
+                edges[r0, c0, neighbors])
+
+            s += crossings # each crossing breaks 1/2 existing sides
+
+        side_count(UP, [LEFT, RIGHT])
+        side_count(DOWN, [LEFT, RIGHT])
+        side_count(LEFT, [UP, DOWN])
+        side_count(RIGHT, [UP, DOWN])
+
         heads = ~used[next_r, next_c] & ~foreign
         hr, hc = next_r[heads], next_c[heads]
         used[hr, hc] = True
 
         for r, c in zip(hr, hc):
-            a, p = grow(r, c, a, p)
+            a, p, s = grow(r, c, a, p, s)
 
-        return a, p
+        return a, p, s
     
     regions = []
     seed = (1,1)
     while seed:
         r0, c0 = seed
-        a, p = grow(r0, c0)
-        regions.append(Region(data[r0, c0], a, p))
+        a, p, s = grow(r0, c0)
+        regions.append(Region(data[r0, c0], a, p, s))
         seed = next_seed(used)
 
     return sorted(regions, key=lambda r: r.tag)
 
 
-def part_1(data):
-    regions = find_regions(data)
-    return sum(r.area*r.perimeter for r in regions)
-
-def part_2(data):
-    return 0
-
-def main(file=None, part=None, verbose=False):
+def main(file=None, part=[1,2]):
 
     with open(file, encoding='utf-8') as f:
         lines = f.readlines()
 
     data = np.array([list(line.strip()) for line in lines])
 
-    if part == 1:
-        total = part_1(data)
-    elif part == 2:
-        total = part_2(data)
-    else:
-        raise ValueError(f'Invalid part number: {part}')
-    
-    if verbose:
-        for i, value in enumerate(values):
-            print(f'{rows[i]}: {value}')
+    regions = find_regions(data)
 
-    return total
+    totals = [sum(r.area*r.perimeter for r in regions),
+              sum(r.area*r.sides for r in regions)]
+
+    if isinstance(part, list):
+        return [totals[p-1] for p in part]
+    else:
+        return totals[part-1]
 
 
 def parse_args():
@@ -95,12 +127,11 @@ def parse_args():
         help='Input file, default: %(default)s')
     parser.add_argument('--part', type=int, nargs='+', default=[1, 2],
         help='Part number(s), default: %(default)s')
-    parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
 if __name__ == '__main__':
 
     args = parse_args()
-    for part in args.part:
-        total = main(args.file, part, args.verbose)
+    totals = main(args.file)
+    for part, total in zip(args.part, totals):
         print(f'Part {part} total: {total}')
--- 2024/day12/day12_test.py
+++ 2024/day12/day12_test.py
@@ -3,7 +3,7 @@
 import os
 import pytest
 import numpy as np
-from day12 import part_1, part_2, main, Region
+from day12 import main, Region
 
 here = os.path.dirname(os.path.realpath(__file__))
 test_data = os.path.join(here, 'day12.test.dat')
@@ -20,8 +20,8 @@ def test_find_regions():
         'BBCC',
         'EEEC'
     )
-    assert find_regions(data) == [Region('A', 4, 10), Region('B', 4, 8),
-        Region('C', 4, 10), Region('D', 1, 4), Region('E', 3, 8)]
+    assert find_regions(data) == [Region('A', 4, 10, 4), Region('B', 4, 8, 4),
+        Region('C', 4, 10, 8), Region('D', 1, 4, 4), Region('E', 3, 8, 4)]
 
     data = char_mat(
         'OOOOO',
@@ -30,11 +30,34 @@ def test_find_regions():
         'OXOXO',
         'OOOOO'
     )
-    assert find_regions(data) == [Region('O', 21, 36), Region('X', 1, 4),
-        Region('X', 1, 4), Region('X', 1, 4), Region('X', 1, 4)]
+    assert find_regions(data) == [Region('O', 21, 36, 20), Region('X', 1, 4, 4),
+        Region('X', 1, 4, 4), Region('X', 1, 4, 4), Region('X', 1, 4, 4)]
+    
+    data = char_mat(
+        'EEEEE',
+        'EXXXX',
+        'EEEEE',
+        'EXXXX',
+        'EEEEE'
+    )
+    assert find_regions(data) == [Region('E', 17, 36, 12),
+                                  Region('X', 4, 10, 4), Region('X', 4, 10, 4)]
+
+    data = char_mat(
+        'AAAAAA',
+        'AAABBA',
+        'AAABBA',
+        'ABBAAA',
+        'ABBAAA',
+        'AAAAAA'
+    )
+    assert find_regions(data) == [Region('A', 28, 24 + 2*8, 12),
+                                  Region('B', 4, 8, 4), Region('B', 4, 8, 4)]
 
 def test_main_1():
     assert main(file=test_data, part=1) == 1930
 
-# def test_main_2():
-#     assert main(file=test_data, part=2) == 0
+def test_main_2():
+    assert main(file=test_data, part=2) == 1206
+
+# 835812 too low
