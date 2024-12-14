91d919e22e22a38f5e3bd3f585dd71f13cd82a0f
Martin HA <martin.herrerias@gmail.com>
2024-12-14 13:57:39 +0000

    broken day 10 test (?)

--- 2024/day10/day10_test.py
+++ 2024/day10/day10_test.py
@@ -3,7 +3,6 @@
 import os
 import pytest
 import numpy as np
-from day10 import part_1, part_2, main
 
 here = os.path.dirname(os.path.realpath(__file__))
 test_data = os.path.join(here, 'day10.test.dat')
@@ -11,10 +10,12 @@ test_data = os.path.join(here, 'day10.test.dat')
 def test_follow_trail():
     from day10 import follow_trail
     data = np.array([[0,1,2],[5,4,3],[6,7,8],[1,0,9]])
-    assert follow_trail(data,0,0) == 1
+    assert follow_trail(data,0,0) == (1,1)
 
 def test_main_1():
+    from day10 import main
     assert main(file=test_data, part=1) == 36
 
 def test_main_2():
+    from day10 import main
     assert main(file=test_data, part=2) == 81
