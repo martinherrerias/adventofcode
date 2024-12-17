01c439bb84e189ed4c9a5a8c57981fc51897a061
Martin HA <martin.herrerias@gmail.com>
2024-12-17 15:59:54 +0000

    Day 17, part 2

--- 2024/day17/day17.py
+++ 2024/day17/day17.py
@@ -5,16 +5,50 @@ Advent of Code 2024 - https://adventofcode.com/2024/
 
 import argparse
 from pathlib import Path
+from collections import defaultdict
+
+MAX_BITS = 128
+
+class BitRange():
+
+    def __init__(self, lo = None, hi = None):
+        self.lo = lo
+        self.hi = hi
+
+    @property
+    def n(self):
+        return self.hi - self.lo
+
+    def update(self, n):
+        self.lo = min(self.lo, n) if self.lo is not None else n
+        self.hi = max(self.hi, n+1) if self.hi is not None else n+1
+
+    def set(self, number, value):
+        """
+        Set bits of number in the range [lo, hi) to value
+        """
+
+        assert value >> self.n == 0
+
+        mask = (1 << self.n) - 1
+        number &= ~(mask << self.lo)
+        number |= value << self.lo
+
+        return number
+    
+    def __repr__(self):
+        return f'BitRange({self.lo}, {self.hi})'
+
 
 class Computer():
 
     def __init__(self, a, b, c, program):
-        self.reg_a = a
-        self.reg_b = b
-        self.reg_c = c
+        self.a = a
+        self.b = b
+        self.c = c
         self.program = program
-        self.pointer = 0
-        self.out = []
+        self.ptr = 0
+        self._out = []
 
     @classmethod
     def from_file(cls, file):
@@ -42,57 +76,110 @@ class Computer():
             7: cls.cdv
         }
 
-    def run(self, verbose = False):
-        while self.pointer < len(self.program):
-            ins = self.program[self.pointer]
-            op = self.program[self.pointer + 1]
+    def run(self, a = None):
+
+        if a is not None:
+            self.a = a
+        
+        self._out = []
+        self.ptr = 0
+        while self.ptr < len(self.program):
+            ins = self.program[self.ptr]
+            op = self.program[self.ptr + 1]
             self.instructions()[ins](self, op)
+
+        return self._out
+
+    def quine(self):
+        """
+        Find a value of A that causes the program to print itself
+        """
+
+        influence = defaultdict(BitRange)
+
+        # Get the range of bits that influence each printed output
+        # influence[j] = BitRange(i,k) means bits [i, k) of A set output[j]
+        for n in range(MAX_BITS):
+            min_len = len(self.run(2**n))
+            max_len = len(self.run(2**(n+1)-1))
+            assert min_len == max_len
+
+            if min_len > len(self.program):
+                break
+
+            influence[min_len-1].update(n)
+
+        # Recursively find values for the bits in influence[j, j-1, ... 0]
+        # that cause the output[j:] to match the program[j:]
+        def _find_output(j, a0):
+
+            bit_range = influence[j]
+
+            for x in range(2**bit_range.n):
+
+                a = bit_range.set(a0, x)
+                out = self.run(a = a)
+
+                if len(out) == len(self.program) and out[j:] == self.program[j:]:
+                    if j == 0:
+                        return a
+                    else:
+                        a = _find_output(j-1, a)
+                        if a is not None:
+                            return a
+                        else:
+                            continue
+
+            return None
+
+        return _find_output(len(self.program)-1, 0)
+
 
     def combo(self, op):
         if 0 <= op <= 3:
             return op
         elif op == 4:
-            return self.reg_a
+            return self.a
         elif op == 5:
-            return self.reg_b
+            return self.b
         elif op == 6:
-            return self.reg_c
+            return self.c
         else:
             raise ValueError('Unknown combo operand')
 
     def adv(self, op):
-        self.reg_a = self.reg_a // 2 ** self.combo(op)
-        self.pointer += 2
+        self.a = self.a // 2 ** self.combo(op)
+        self.ptr += 2
 
     def bxl(self, op):
-        self.reg_b = self.reg_b ^ op
-        self.pointer += 2
+        self.b = self.b ^ op
+        self.ptr += 2
 
     def bst(self, op):
-        self.reg_b = self.combo(op) % 8
-        self.pointer += 2
+        self.b = self.combo(op) % 8
+        self.ptr += 2
 
     def jnz(self, op):
-        if self.reg_a == 0:
-            self.pointer += 2
+        if self.a == 0:
+            self.ptr += 2
         else:
-            self.pointer = op
+            self.ptr = op
 
     def bxc(self, op):
-        self.reg_b = self.reg_b ^ self.reg_c
-        self.pointer += 2
+        self.b = self.b ^ self.c
+        self.ptr += 2
 
     def out(self, op):
-        self.out.append(self.combo(op) % 8)
-        self.pointer += 2
+        self._out.append(self.combo(op) % 8)
+        self.ptr += 2
 
     def bdv(self, op):
-        self.reg_b = self.reg_a // 2 ** self.combo(op)
-        self.pointer += 2
+        self.b = self.a // 2 ** self.combo(op)
+        self.ptr += 2
 
     def cdv(self, op):
-        self.reg_c = self.reg_a // 2 ** self.combo(op)
-        self.pointer += 2
+        self.c = self.a // 2 ** self.combo(op)
+        self.ptr += 2
 
 
 def parse_args():
@@ -101,18 +188,16 @@ def parse_args():
         help='Input file, default: %(default)s')
     parser.add_argument('--part', type=int, nargs='+', default=[1, 2],
         help='Part number(s), default: %(default)s')
-    parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
-def main(file=None, part=None, verbose=False):
+def main(file=None, part=None):
 
     cmp = Computer.from_file(file)
-    cmp.run(verbose=verbose)
 
     if part == 1:
-        return ','.join([str(x) for x in cmp.out])
+        return ','.join([str(x) for x in cmp.run()])
     elif part == 2:
-        return None
+        return cmp.quine()
     else:
         raise ValueError(f'Invalid part number: {part}')
 
@@ -120,5 +205,5 @@ if __name__ == '__main__':
 
     args = parse_args()
     for part in args.part:
-        total = main(args.file, part, args.verbose)
-        print(f'Part {part} total: {total}')
+        total = main(args.file, part)
+        print(f'Part {part}: {total}')
--- 2024/day17/day17_test.py
+++ 2024/day17/day17_test.py
@@ -11,45 +11,42 @@ def test_individual():
 
     cmp = Computer(0, 0, 9, [2, 6])
     cmp.run()
-    assert cmp.reg_b == 1
+    assert cmp.b == 1
 
     cmp = Computer(10, 0, 0, [5, 0, 5, 1, 5, 4])
-    cmp.run()
-    assert cmp.out == [0, 1, 2]
+    assert cmp.run() == [0, 1, 2]
 
     cmp = Computer(2024, 0, 0, [0, 1, 5, 4, 3, 0])
-    cmp.run()
-    assert cmp.out == [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]
-    assert cmp.reg_a == 0
+    assert cmp.run() == [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]
+    assert cmp.a == 0
 
     cmp = Computer(0, 29, 0, [1, 7])
     cmp.run()
-    assert cmp.reg_b == 26
+    assert cmp.b == 26
 
     cmp = Computer(0, 2024, 43690, [4, 0])
     cmp.run()
-    assert cmp.reg_b == 44354
+    assert cmp.b == 44354
 
     # not in docs, for full coverage
     cmp = Computer(125, 3, 2, [6, 5, 7, 6])
     cmp.run()
-    assert cmp.reg_a == 125
-    assert cmp.reg_b == 15
-    assert cmp.reg_c == 31
+    assert cmp.a == 125
+    assert cmp.b == 15
+    assert cmp.c == 31
 
 
 def test_part_1():
     from day17 import Computer
     cmp = Computer.from_file(test_data)
-    cmp.run()
-    assert cmp.out == [4,6,3,5,6,3,5,2,1,0]
+    assert cmp.run() == [4,6,3,5,6,3,5,2,1,0]
 
-# def test_part_2():
-#     assert part_2(np.array([1, 2, 3])) == 12
+def test_part_2():
+    from day17 import Computer
+    cmp = Computer(0,0,0,[0,3,5,4,3,0])
+    assert cmp.run(a = 117440) == [0,3,5,4,3,0]
+    assert cmp.quine() == 117440
 
 def test_main_1():
     from day17 import main
     assert main(file=test_data, part=1) == '4,6,3,5,6,3,5,2,1,0'
-
-# def test_main_2():
-#     assert main(file=test_data, part=2) == 360
