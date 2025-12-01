11a40c5f6798f688787930e7d363a4f426ebcf50
Martin HA <martin.herrerias@gmail.com>
2025-12-01 09:30:13 +0000

    2025 templates

--- 2025/template/template.py
+++ 2025/template/template.py
@@ -1,25 +1,36 @@
 #! /usr/bin/env python3
-'''
-Advent of Code 2024 - https://adventofcode.com/2024/
-'''
+"""
+Advent of Code 2025 - https://adventofcode.com/2025/
+"""
 
 import argparse
 from pathlib import Path
 
 import numpy as np
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file', type=str, default=Path(__file__).with_suffix('.dat'), 
-        help='Input file, default: %(default)s')
-    parser.add_argument('--part', type=int, nargs='+', default=[1, 2],
-        help='Part number(s), default: %(default)s')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument(
+        "--file",
+        type=str,
+        default=Path(__file__).with_suffix(".dat"),
+        help="Input file, default: %(default)s",
+    )
+    parser.add_argument(
+        "--part",
+        type=int,
+        nargs="+",
+        default=[1, 2],
+        help="Part number(s), default: %(default)s",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")
     return parser.parse_args()
 
 
 def part_1(row):
     return sum(row)
+
 
 def part_2(row):
     return sum(row) * 2
@@ -27,18 +38,18 @@ def part_2(row):
 
 def main(file=None, part=None, verbose=False):
 
-    with open(file, encoding='utf-8') as f:
+    with open(file, encoding="utf-8") as f:
         lines = f.readlines()
 
     # Numeric row-wise
-    rows = [np.fromstring(line, dtype=int, sep=' ') for line in lines]
+    rows = [np.fromstring(line, dtype=int, sep=" ") for line in lines]
 
     if part == 1:
         values = [part_1(row) for row in rows]
     elif part == 2:
         values = [part_2(row) for row in rows]
     else:
-        raise ValueError(f'Invalid part number: {part}')
+        raise ValueError(f"Invalid part number: {part}")
 
     total = sum(values)
 
@@ -51,17 +62,17 @@ def main(file=None, part=None, verbose=False):
     #     total = part_2(data)
     # else:
     #     raise ValueError(f'Invalid part number: {part}')
-    
+
     if verbose:
         for i, value in enumerate(values):
-            print(f'{rows[i]}: {value}')
+            print(f"{rows[i]}: {value}")
 
     return total
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     args = parse_args()
     for part in args.part:
         total = main(args.file, part, args.verbose)
-        print(f'Part {part} total: {total}')
+        print(f"Part {part} total: {total}")
--- 2025/template/template_test.py
+++ 2025/template/template_test.py
@@ -1,21 +1,25 @@
 # Unit tests for src/template.py
 
 import os
-import pytest
+# import pytest
 import numpy as np
 from template import part_1, part_2, main
 
 here = os.path.dirname(os.path.realpath(__file__))
-test_data = os.path.join(here, 'template.test.dat')
+test_data = os.path.join(here, "template.test.dat")
+
 
 def test_part_1():
     assert part_1(np.array([1, 2, 3])) == 6
 
+
 def test_part_2():
     assert part_2(np.array([1, 2, 3])) == 12
 
+
 def test_main_1():
     assert main(file=test_data, part=1) == 180
+
 
 def test_main_2():
     assert main(file=test_data, part=2) == 360
