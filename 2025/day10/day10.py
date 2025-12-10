78632f9cc2d8da3361708f36d9627229a5fc0d6e
Martin HA <martin.herrerias@gmail.com>
2025-12-10 10:41:52 +0000

    Day10, part 1

---
+++ 2025/day10/day10.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python3
+"""
+Advent of Code 2025, day 10
+https://adventofcode.com/2025/day/10
+"""
+
+import re
+import argparse
+from pathlib import Path
+
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        default=Path(__file__).with_suffix(".dat"),
+        help="Input file, default: %(default)s",
+    )
+    parser.add_argument(
+        "-p",
+        "--part",
+        type=int,
+        nargs="+",
+        default=[1, 2],
+        help="Part number(s), default: %(default)s",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")
+    return parser.parse_args()
+
+
+def part_1(line):
+    wiring_matrix, light_diagram, _ = parse_line(line)
+    return min_switches(wiring_matrix, light_diagram)
+
+
+def parse_line(line: str):
+    data = re.match(r"\[(.+)\] (.+) \{(.+)\}", line).groups()
+
+    light_diagram = np.array([x == "#" for x in data[0]])
+    button_schematics = [
+        tuple(map(int, x.split(","))) for x in data[1].strip("()").split(") (")
+    ]
+    joltage = tuple(map(int, data[2].split(",")))
+
+    wiring_matrix = np.zeros((len(light_diagram), len(button_schematics)), dtype=bool)
+    for b, s in enumerate(button_schematics):
+        wiring_matrix[s, b] = True
+
+    return wiring_matrix, light_diagram, joltage
+
+
+def min_switches(wiring_matrix, light_diagram, _state=tuple()):
+
+    if len(_state) == len(light_diagram):
+        pass
+
+    # we can flip them off the same as we flipped them on
+    need_switching = light_diagram
+    if not any(need_switching):
+        return 0
+
+    if wiring_matrix.shape[1] > 1:
+
+        # Is it even doable?
+        # there should be switches to flip the missing lights
+        if not all(np.any(wiring_matrix[need_switching, :], axis=1)):
+            return np.nan
+
+        # steps without flipping first switch
+        without_first_switch = min_switches(wiring_matrix[:, 1:], light_diagram, _state + (0,))
+
+        # steps if flipping first switch
+        new_state = np.bitwise_xor(wiring_matrix[:, 0], light_diagram)
+        with_first_switch = 1 + min_switches(wiring_matrix[:, 1:], new_state, _state + (1,))
+
+        return np.nanmin([without_first_switch, with_first_switch])
+
+    elif np.all(wiring_matrix[:, 0] == light_diagram):  # flip one last switch
+        return 1
+    else:
+        return np.nan
+
+
+def main(file=None, part=None, verbose=False):
+
+    with open(file, encoding="utf-8") as f:
+        lines = f.readlines()
+
+    if part == 1:
+        values = [part_1(line) for line in lines if line.strip()]
+    elif part == 2:
+        NotImplementedError()
+    else:
+        raise ValueError(f"Invalid part number: {part}")
+
+    total = sum(values)
+
+    return total
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    for part in args.part:
+        total = main(args.file, part, args.verbose)
+        print(f"Part {part} total: {total}")
---
+++ 2025/day10/day10_test.py
@@ -0,0 +1,31 @@
+# Unit tests for src/day10.py
+
+import os
+
+# import pytest
+import numpy as np
+
+here = os.path.dirname(os.path.realpath(__file__))
+test_data = os.path.join(here, "day10.test.dat")
+
+
+def test_parse_line():
+    from day10 import parse_line
+
+    m, s, j = parse_line("[.#.] (0) (1,2) {1,2,3}")
+    assert np.array_equal(m, [[1, 0], [0, 1], [0, 1]])
+    assert np.array_equal(s, [0, 1, 0])
+    assert np.array_equal(j, [1, 2, 3])
+
+
+def test_part_1():
+    from day10 import part_1
+
+    assert part_1("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}") == 2
+    assert part_1("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}") == 3
+    assert part_1("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}") == 2
+
+
+def test_main_1():
+    from day10 import main
+    assert main(file=test_data, part=1) == 7
